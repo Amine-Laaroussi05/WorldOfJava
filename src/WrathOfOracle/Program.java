6d1ef4dbd60cef1977abcca62d0cb345f1e34ac0
Amine Laaroussi <amine.laaroussi05@gmail.com>
2022-05-14 15:55:55 +0200

    WOJ: création du monde

--- src/WrathOfOracle/Program.java
+++ src/WrathOfOracle/Program.java
@@ -3,19 +3,36 @@ package WrathOfOracle;
 import java.util.Scanner;
 
 public class Program {
-    static Scanner scanner = new Scanner(System.in);
 
-    /**
-     * Method: créer un personnage.
-     * Demande à l'utilisateur de saisir son nom.
-     * crée une classe personnage en initialisant les points de vie à 0, les dégats à 0 et avec le nom saisi.
-     * @return une instance de la classe Personnage correctement instancié.
-     */
-    public static Personnage personnageFactory(){
-        String nom = Tools.inputString("Saisir un nom de personnage: ");
-        Personnage personnage = new Personnage(0,0,nom);
-        return personnage;
+    static class Monde{
+
+        /**
+         * Method: créer un personnage.
+         * Demande à l'utilisateur de saisir son nom.
+         * crée une classe personnage en initialisant les points de vie à 0, les dégats à 0 et avec le nom saisi.
+         * @return une instance de la classe Personnage correctement instancié.
+         */
+        public static Personnage personnageFactory(){
+            String nom = Tools.inputString("Saisir un nom de personnage: ");
+            Personnage personnage = new Personnage(0,0,nom);
+            return personnage;
+        }
+
+
+        /**
+         * Method: affiche les informations du monde
+         * Pour l'instant, la méthode affiche seulement le personnage
+         * @param personnage
+         */
+        public static void afficherInformations(Personnage personnage){
+            personnage.toString();
+        }
+
     }
+
+
+
+
 
 
 
